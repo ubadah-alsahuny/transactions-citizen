5319330290c739b2a3478c62fab3bf990c029ace
Ubadah Alsahuny <ubadahalsahuny2003@gmail.com>
2026-04-19 12:23:56 +0300

    Removed more unnecessary folders and files related to other sections of the project, which are included in other repositories.

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -5,8 +5,6 @@ import Login from '../pages/auth/Login'
 import Register from '../pages/auth/Register'
 
 import CitizenLayout from '../layouts/CitizenLayout'
-import OfficerLayout from '../layouts/OfficerLayout'
-import AdminLayout from '../layouts/AdminLayout'
 
 import CitizenDashboard from '../pages/citizen/Dashboard/DashboardPage.tsx'
 import NewTransactionPage from '../pages/citizen/NewTransaction/NewTransactionPage.tsx'
@@ -14,14 +12,7 @@ import TransactionDetailsPage from '../pages/citizen/TransactionDetails/Transact
 import MyDocumentsPage from '../pages/citizen/MyDocuments/MyDocumentsPage.tsx'
 import AccountPage from "../pages/citizen/Account/AccountPage.tsx";
 
-import OfficerDashboard from '../pages/officer/Dashboard'
-import ReviewTransaction from '../pages/officer/ReviewTransaction'
-
-import AdminDashboard from '../pages/admin/Dashboard'
-import UserManagement from '../pages/admin/UserManagement'
-
 import NotFound from '../pages/system/NotFound'
-
 
 export const router = createBrowserRouter([
     { path: '/', element: <Landing /> },
@@ -37,24 +28,6 @@ export const router = createBrowserRouter([
             { path: 'transaction/:id', element: <TransactionDetailsPage /> },
             { path: 'documents', element: <MyDocumentsPage /> },
             { path: 'account', element: <AccountPage />}
-        ]
-    },
-
-    {
-        path: '/officer',
-        element: <OfficerLayout />,
-        children: [
-            { path: 'dashboard', element: <OfficerDashboard /> },
-            { path: 'review/:id', element: <ReviewTransaction /> }
-        ]
-    },
-
-    {
-        path: '/admin',
-        element: <AdminLayout />,
-        children: [
-            { path: 'dashboard', element: <AdminDashboard /> },
-            { path: 'users', element: <UserManagement /> }
         ]
     },
 
